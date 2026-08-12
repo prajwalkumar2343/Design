0afe558e1aab6d1c53c0499360e58a2ef9045e3c
Prajwal Kumar <prajwalkumar2343@users.noreply.github.com>
2026-08-13 01:19:35 +0530

    Add src/shaders/index.ts

---
+++ src/shaders/index.ts
@@ -0,0 +1,14 @@
+export {
+  PAPER_SHADER_DEFINITIONS,
+  PAPER_SHADER_IDS,
+  UnsupportedPaperShaderError,
+  detectPaperShaderSupport,
+  getPaperShaderDefinition,
+  isPaperShaderId,
+  loadPaperShader,
+  type DetectPaperShaderSupportOptions,
+  type LoadedPaperShader,
+  type PaperShaderDefinition,
+  type PaperShaderId,
+  type PaperShaderSupport,
+} from "./registry";

3f4c36c03fcc59de0bfc62656e32267e3fc9fd5a
Prajwal Kumar <prajwalkumar2343@users.noreply.github.com>
2026-08-13 01:19:35 +0530

    Add src/voice/index.ts

---
+++ src/voice/index.ts
@@ -0,0 +1,8 @@
+export {
+  BrowserAudioCapture,
+  BrowserAudioCaptureError,
+  type BrowserAudioCaptureErrorCode,
+  type BrowserAudioCaptureOptions,
+  type BrowserAudioCapturePhase,
+} from "./browser-audio-capture";
+
